e0bddf048fed26d8c690feb05c451948c49c0da1
Harry Fahringer III <viccem.dev@gmail.com>
2022-01-21 14:25:21 -0500

    - Added z offset to control size of entity being drawn
    - Mapped noise to randomize size of entity being drawn

--- p5/src/perlin.js
+++ p5/src/perlin.js
@@ -1,13 +1,13 @@
 let xoff = 0
 let yoff = 0
-let yoffText
-let xoffText
+let zoff = 0
 let _x = 0
 let _y = 0
+let _z = 0
+let minSize = 3 // see bottom for settings
+let maxSize = 6 // ^
 
-// TODO: Find a way to display returns of noise functions
-
-console.log('perlin.js')
+// console.log('perlin.js') // LOG
 
 function setup() {
   createCanvas(400, 400)
@@ -23,23 +23,32 @@ function draw() {
   // map the result xoff and yoff between 0 and width, 0 and height
   let x = map(noise(xoff), 0, 1, 0, width)
   let y = map(noise(yoff), 0, 1, 0, height)
+  let z = map(noise(zoff), 0, 1, minSize, maxSize)
   
   // change result of noise(off)
-  xoff += 0.008 // >0.001 strobes, creates jerky movement
-  yoff += 0.008 
+  // xoff += 0.008 // >0.001 strobes, creates jerky movement
+  // yoff += 0.008 
+
+  xoff += map(noise(xoff), 0, 1, 0.0001, 0.006)
+  // yoff += map(noise(yoff), 0, 1, 0.0001, 0.008) 
+  yoff += map(noise(yoff), 0, 1, 0.008, 0.0001)
+
+  zoff += map(noise(zoff), 0, 1, minSize, maxSize)
   
   // draw an ellipse
-  ellipse(x, y, 24, 24)
+  ellipse(x, y, z, z)
   
   // update _x and _y
   _x = createP('X Offset ' + x)
   _y = createP('Y Offset: ' + y)
+  _x = createP('Z Offset', + z)
 
 }
 
-// // display x and y offsets on screen for users' reference
-// function displayOffsets() {
-//     xoffText = createP('X Offset ' + _x)
-//   yoffText = createP('Y Offset: ' + _y)
-// }
+// #-
 
+  // minSize && maxSize settings
+  // -
+  // Firefly: minSize = 3, maxSize = 6
+
+// -#
