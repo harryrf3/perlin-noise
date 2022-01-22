9e58ac4d9fd0f1c5f9dc2e265870662060ac5a20
Harry Fahringer III <viccem.dev@gmail.com>
2022-01-22 01:19:19 -0500

    - Created a 1d wave graph based on perlin noise.
    *Remove comment from noLoop() to see code animated.*

--- p5/src/perlin.js
+++ p5/src/perlin.js
@@ -1,3 +1,6 @@
+// let xoff1 = 0
+// let xoff2 = 1000
+
 let xoff = 0
 
 // console.log('perlin.js') // LOG
@@ -7,18 +10,32 @@ function setup() {
 }
 
 function draw() {
-  
   background(51)
-  
-  // map the result xoff and yoff between 0 and width, 0 and height
-  let x = map(noise(xoff), 0, 1, 0, width)
-  let y = map(noise(xoff), 0, 1, 0, height)
-  
-  // change result of noise(off)
-  xoff += 0.008 // >0.001 strobes, creates jerky movement
-  
-  // draw an ellipse
-  ellipse(x, y, 24, 24)
 
+  stroke(255)
+  noFill()
+  beginShape()
+  for(var x = 0; x < width; x++) {
+    stroke(255)
+    // let y = random(height)
+    let y = noise(xoff) * height
+    vertex(x, y)
+
+    xoff += 0.02
+  }
+  endShape()
+
+  noLoop()
+
+  // // map the value (0 and 1) of noise(xoff) between 0 and width, 0 and height
+  // let x = map(noise(xoff1), 0, 1, 0, width)
+  // let y = map(noise(xoff2), 0, 1, 0, height)
+  
+  // // add to value of xoff for next move
+  // xoff1 += 0.008 // >0.001 strobes, creates jerky movement
+  // xoff2 += 0.008 // >0.001 strobes, creates jerky movement
+  
+  // // draw an ellipse
+  // ellipse(x, y, 24, 24)
 }
 
